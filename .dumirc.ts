23f7efedddb360ff63b88a1665953f1df0d48014
DESKTOP-31IBRMI\Administrator <1509326266@qq.com>
2024-08-14 13:32:15 +0800

    fix(build): fixed webpack import plugin failure

--- .dumirc.ts
+++ .dumirc.ts
@@ -14,7 +14,6 @@ const collectionName = VITE_ICON_LOCAL_PREFIX.replace(`${VITE_ICON_PREFIX}-`, ''
 
 export default defineConfig({
   outputPath: 'docs-dist',
-  mako: {},
   hash: true,
   crossorigin: {},
   exportStatic: {},
@@ -55,63 +54,5 @@ export default defineConfig({
       })
     );
   },
-  extraRemarkPlugins: [remarkAntd],
-  headScripts: [
-    `
-    (function () {
-      function isLocalStorageNameSupported() {
-        const testKey = 'test';
-        const storage = window.localStorage;
-        try {
-          storage.setItem(testKey, '1');
-          storage.removeItem(testKey);
-          return true;
-        } catch (error) {
-          return false;
-        }
-      }
-      // 优先级提高到所有静态资源的前面，语言不对，加载其他静态资源没意义
-      const pathname = location.pathname;
-
-      function isZhCN(pathname) {
-        return /-cn\\/?$/.test(pathname);
-      }
-      function getLocalizedPathname(path, zhCN) {
-        const pathname = path.indexOf('/') === 0 ? path : '/' + path;
-        if (!zhCN) {
-          // to enUS
-          return /\\/?index(-cn)?/.test(pathname) ? '/' : pathname.replace('-cn', '');
-        } else if (pathname === '/') {
-          return '/index-cn';
-        } else if (pathname.indexOf('/') === pathname.length - 1) {
-          return pathname.replace(/\\/$/, '-cn/');
-        }
-        return pathname + '-cn';
-      }
-
-      // 兼容旧的 URL， \`?locale=...\`
-      const queryString = location.search;
-      if (queryString) {
-        const isZhCNConfig = queryString.indexOf('zh-CN') > -1;
-        if (isZhCNConfig && !isZhCN(pathname)) {
-          location.pathname = getLocalizedPathname(pathname, isZhCNConfig);
-        }
-      }
-
-      // 首页无视链接里面的语言设置 https://github.com/ant-design/ant-design/issues/4552
-      if (isLocalStorageNameSupported() && (pathname === '/' || pathname === '/index-cn')) {
-        const lang =
-          (window.localStorage && localStorage.getItem('locale')) ||
-          ((navigator.language || navigator.browserLanguage).toLowerCase() === 'zh-cn'
-            ? 'zh-CN'
-            : 'en-US');
-        // safari is 'zh-cn', while other browser is 'zh-CN';
-        if ((lang === 'zh-CN') !== isZhCN(pathname)) {
-          location.pathname = getLocalizedPathname(pathname, lang === 'zh-CN');
-        }
-      }
-      document.documentElement.className += isZhCN(pathname) ? 'zh-cn' : 'en-us';
-    })();
-    `
-  ]
+  extraRemarkPlugins: [remarkAntd]
 });
