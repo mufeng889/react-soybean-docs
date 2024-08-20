cae42d43894d418a0d625a2204cb12b1f11db520
DESKTOP-31IBRMI\Administrator <1509326266@qq.com>
2024-08-20 11:34:51 +0800

    feat(dumi): improve Footer

--- .dumi/theme/slots/Footer/index.tsx
+++ .dumi/theme/slots/Footer/index.tsx
@@ -1,10 +1,20 @@
 import { TinyColor } from '@ctrl/tinycolor';
 import { createStyles } from 'antd-style';
 import getAlphaColor from 'antd/es/theme/util/getAlphaColor';
-import { FormattedMessage } from 'dumi';
+import { FormattedMessage,Link } from 'dumi';
 import RcFooter from 'rc-footer';
 import { useContext } from 'react';
 import SiteContext from '../SiteContext';
+import useLocation from '../../../hooks/useLocation';
+
+import {
+  BugOutlined,
+  GithubOutlined,
+  HistoryOutlined,
+  IssuesCloseOutlined,
+  MessageOutlined,
+  QuestionCircleOutlined,
+} from '@ant-design/icons';
 
 const useStyle = () => {
   const { isMobile } = useContext(SiteContext);
@@ -29,7 +39,7 @@ const useStyle = () => {
         }
 
         .rc-footer-column {
-          margin-bottom: ${isMobile ? 60 : 0}px;
+          margin-bottom: ${isMobile ? 40 : 0}px;
           :last-child {
             margin-bottom: ${isMobile ? 20 : 0}px;
           }
@@ -42,7 +52,7 @@ const useStyle = () => {
         .rc-footer-container {
           max-width: 1208px;
           margin-inline: auto;
-          padding-inline: ${token.marginXXL}px;
+          padding: 40px 0 20px;
         }
 
         .rc-footer-bottom {
@@ -56,57 +66,146 @@ const useStyle = () => {
   })();
 };
 
+
+const col1 = {
+  title: <FormattedMessage id="soy-series" />,
+  items: [
+    {
+      title: 'React Soybean',
+      url: 'https://github.com/mufeng889/react-soybean-admin',
+      openExternal: true,
+    },
+    {
+      title: 'React Soybean',
+      description: <FormattedMessage id="online-preview" />,
+      url: 'https://react-soybean.ohh-889.com/',
+      openExternal: true,
+    },
+    {
+      title: 'React Soybean',
+      description: <FormattedMessage id="app.footer.chinamirror" />,
+      url: 'https://react-soybean-admin.pages.dev/',
+      openExternal: true,
+    },
+    {
+      title: 'React Soybean Docs',
+      url: 'https://github.com/mufeng889/react-soybean-docs',
+      openExternal: true,
+    },
+    {
+      title: 'Soybean',
+      url: 'https://github.com/soybeanjs/soybean-admin',
+      openExternal: true,
+    },
+    {
+      title: 'Elegant Router',
+      description: <FormattedMessage id="auto-router" />,
+      url: 'https://github.com/soybeanjs/elegant-router',
+      openExternal: true,
+    },
+    {
+      title: 'Eslint Config',
+      description: <FormattedMessage id="out-box" />,
+      url: 'https://github.com/soybeanjs/eslint-config',
+      openExternal: true,
+    },
+  ],
+};
+
+const col2 = {
+  title: <FormattedMessage id="app.footer.resources" />,
+  items: [
+    {
+      title: 'Ant Design Charts',
+      url: 'https://ant-design-charts.antgroup.com',
+      openExternal: true,
+    },
+    {
+      title: 'Ant Design Pro Components',
+      url: 'https://procomponents.ant.design',
+      openExternal: true,
+    },
+    {
+      title: 'Umi',
+      description: <FormattedMessage id="app.footer.umi" />,
+      url: 'https://umijs.org',
+      openExternal: true,
+    },
+    {
+      title: 'dumi',
+      description: <FormattedMessage id="app.footer.dumi" />,
+      url: 'https://d.umijs.org',
+      openExternal: true,
+    },
+    {
+      title: 'qiankun',
+      description: <FormattedMessage id="app.footer.qiankun" />,
+      url: 'https://qiankun.umijs.org',
+      openExternal: true,
+    },
+    {
+      title: 'ahooks',
+      description: <FormattedMessage id="app.footer.hooks" />,
+      url: 'https://github.com/alibaba/hooks',
+      openExternal: true,
+    },
+    {
+      title: <FormattedMessage id="app.footer.chinamirror" />,
+      url: 'https://ant-design.antgroup.com',
+    },
+  ],
+};
+
 const Footer = () => {
   const { styles } = useStyle();
 
-  const col1 = {
-    title: <FormattedMessage id="app.footer.resources" />,
+  const location = useLocation();
+
+
+
+  const { getLink } = location;
+
+  const col3 = {
+    title: <FormattedMessage id="app.footer.help" />,
     items: [
       {
-        title: 'Ant Design Charts',
-        url: 'https://ant-design-charts.antgroup.com',
+        icon: <GithubOutlined />,
+        title: 'GitHub',
+        url: 'https://github.com/mufeng889/react-soybean-admin',
         openExternal: true,
       },
       {
-        title: 'Ant Design Pro Components',
-        url: 'https://procomponents.ant.design',
+        icon: <QuestionCircleOutlined />,
+        title: <FormattedMessage id="app.footer.faq" />,
+        url: getLink('/docs/faq'),
+        LinkComponent: Link,
+      },
+      {
+        icon: <BugOutlined />,
+        title: <FormattedMessage id="app.footer.bug-report" />,
+        url: 'https://github.com/mufeng889/react-soybean-admin/issues/new?assignees=&labels=bug%3F&projects=&template=bug-report_cn.yaml&title=%5BBug%5D%3A+',
         openExternal: true,
       },
       {
-        title: 'Umi',
-        description: <FormattedMessage id="app.footer.umi" />,
-        url: 'https://umijs.org',
+        icon: <IssuesCloseOutlined />,
+        title: <FormattedMessage id="app.footer.issues" />,
+        url: 'https://github.com/mufeng889/react-soybean-admin/issues',
         openExternal: true,
       },
       {
-        title: 'dumi',
-        description: <FormattedMessage id="app.footer.dumi" />,
-        url: 'https://d.umijs.org',
+        icon: <MessageOutlined />,
+        title: <FormattedMessage id="app.footer.discussions" />,
+        url: 'https://github.com/mufeng889/react-soybean-admin/discussions',
         openExternal: true,
-      },
-      {
-        title: 'qiankun',
-        description: <FormattedMessage id="app.footer.qiankun" />,
-        url: 'https://qiankun.umijs.org',
-        openExternal: true,
-      },
-      {
-        title: 'ahooks',
-        description: <FormattedMessage id="app.footer.hooks" />,
-        url: 'https://github.com/alibaba/hooks',
-        openExternal: true,
-      },
-      {
-        title: <FormattedMessage id="app.footer.chinamirror" />,
-        url: 'https://ant-design.antgroup.com',
       },
     ],
   };
 
+
   return (
     <RcFooter
       className={styles.footer}
-      columns={[col1]}
+      columns={[col1,col2,col3]}
       bottom={
         <>
           <div style={{ opacity: '0.4' }}>
